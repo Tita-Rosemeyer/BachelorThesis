ae986fb3f43d5d57b6036bd20f60a73eafae7038
Tita Rosemeyer <tita-philine.rosemeyer@inria.fr>
2024-02-06 12:58:56 +0100

    comment freertos in mathext (otherwise it crashes)

--- src/heptagon/c/mathext.c
+++ src/heptagon/c/mathext.c
@@ -1,6 +1,6 @@
 #include <math.h>
 #include "mathext_types.h"
-#include "FreeRTOS.h"
+//#include "FreeRTOS.h"
 #include <stdint.h>
 // #include "task.h"
 
@@ -85,4 +85,130 @@ void Mathext__invSqrt_step(float x, Mathext__invSqrt_out* _out) {
 
 void Mathext__xTaskGetTickCount_step(Mathext__xTaskGetTickCount_out* _out) {
   _out->tick = xTaskGetTickCount();
+}
+
+// Kalman filter
+#include "static_mem.h"
+#include "arm_math.h"
+#include "kalman_core.h"
+
+void Mathext__arm_sqrt_step(float x, Mathext__arm_sqrt_out* _out) {
+  _out->y = arm_sqrt(x);
+}
+
+void Mathext__arm_cos_f32_step(float x, Mathext__arm_cos_f32_out* _out) {
+  _out->y = arm_cos_f32(x);
+}
+
+void Mathext__arm_sin_f32_step(float x, Mathext__arm_sin_f32_out* _out) {
+  _out->y = arm_sin_f32(x);
+}
+
+void quadrocpter_to_array(Mathext__quadrocopter_state* q, float p_array[9]) {
+  p_array[0] = q->kc_state_x;
+  p_array[1] = q->kc_state_y;
+  p_array[2] = q->kc_state_z;
+  p_array[3] = q->kc_state_px;
+  p_array[4] = q->kc_state_py;
+  p_array[5] = q->kc_state_pz;
+  p_array[6] = q->kc_state_d0;
+  p_array[7] = q->kc_state_d1;
+  p_array[8] = q->kc_state_d2;
+}
+
+void covariance_matrix_to_matrix(Mathext__covariance_matrix* p, float p_array[9][9]) {
+  quadrocpter_to_array(&p->kc_state_X, p_array[0]);
+  quadrocpter_to_array(&p->kc_state_Y, p_array[1]);
+  quadrocpter_to_array(&p->kc_state_Z, p_array[2]);
+  quadrocpter_to_array(&p->kc_state_PX, p_array[3]);
+  quadrocpter_to_array(&p->kc_state_PY, p_array[4]);
+  quadrocpter_to_array(&p->kc_state_PZ, p_array[5]);
+  quadrocpter_to_array(&p->kc_state_D0, p_array[6]);
+  quadrocpter_to_array(&p->kc_state_D1, p_array[7]);
+  quadrocpter_to_array(&p->kc_state_D2, p_array[8]);
+}
+
+void array_to_quadrocpter(float p_array[9], Mathext__quadrocopter_state* q) {
+  q->kc_state_x = p_array[0];
+  q->kc_state_y = p_array[1];
+  q->kc_state_z = p_array[2];
+  q->kc_state_px = p_array[3];
+  q->kc_state_py = p_array[4];
+  q->kc_state_pz = p_array[5];
+  q->kc_state_d0 = p_array[6];
+  q->kc_state_d1 = p_array[7];
+  q->kc_state_d2 = p_array[8];
+}
+
+void matrix_to_covariance_matrix(float p_array[9][9], Mathext__covariance_matrix* p) {
+  array_to_quadrocpter(p_array[0], &p->kc_state_X);
+  array_to_quadrocpter(p_array[1], &p->kc_state_Y);
+  array_to_quadrocpter(p_array[2], &p->kc_state_Z);
+  array_to_quadrocpter(p_array[3], &p->kc_state_PX);
+  array_to_quadrocpter(p_array[4], &p->kc_state_PY);
+  array_to_quadrocpter(p_array[5], &p->kc_state_PZ);
+  array_to_quadrocpter(p_array[6], &p->kc_state_D0);
+  array_to_quadrocpter(p_array[7], &p->kc_state_D1);
+  array_to_quadrocpter(p_array[8], &p->kc_state_D2);
+}
+
+void covariance_update(Mathext__covariance_matrix* am, Mathext__covariance_matrix* p) {
+
+  __attribute__((aligned(4))) static float p_array[KC_STATE_DIM][KC_STATE_DIM];
+  __attribute__((aligned(4))) static float am_array[KC_STATE_DIM][KC_STATE_DIM];
+  // convert Mathext__covariance_matrix to matrix
+  covariance_matrix_to_matrix(p, p_array);
+  covariance_matrix_to_matrix(am, am_array);
+
+  // Initialize arm_matrix_instances for the matrices
+  static arm_matrix_instance_f32 Am = {KC_STATE_DIM, KC_STATE_DIM, (float *)am_array};
+  static arm_matrix_instance_f32 Pm = {KC_STATE_DIM, KC_STATE_DIM, (float *)p_array};
+
+  // Temporary matrices for the covariance updates
+  NO_DMA_CCM_SAFE_ZERO_INIT static float tmpNN1d[KC_STATE_DIM * KC_STATE_DIM];
+  static __attribute__((aligned(4))) arm_matrix_instance_f32 tmpNN1m = { KC_STATE_DIM, KC_STATE_DIM, tmpNN1d};
+
+  NO_DMA_CCM_SAFE_ZERO_INIT static float tmpNN2d[KC_STATE_DIM * KC_STATE_DIM];
+  static __attribute__((aligned(4))) arm_matrix_instance_f32 tmpNN2m = { KC_STATE_DIM, KC_STATE_DIM, tmpNN2d};
+
+  mat_trans(&Am, &tmpNN2m); // A'
+  mat_mult(&Am, &Pm, &tmpNN1m); // A P
+  mat_mult(&tmpNN1m, &tmpNN2m, &Pm); // A P A'
+
+  // convert matrix to Mathext__covariance_matrix
+  matrix_to_covariance_matrix(p_array, p);
+}
+
+void Mathext__covariance_update_step(Mathext__covariance_matrix am, Mathext__covariance_matrix p, Mathext__covariance_update_out* _out) {
+  // update covariance matrix
+  covariance_update(&am, &p);
+  _out->p = p;
+}
+
+#define MAX_COVAR (100)
+#define MIN_COVAR (1e-6f)
+
+void Mathext__enforce_covariance_matrix_symmetry_step(Mathext__covariance_matrix p, Mathext__enforce_covariance_matrix_symmetry_out* _out) {
+  __attribute__((aligned(4))) float p_array[KC_STATE_DIM][KC_STATE_DIM];
+  covariance_matrix_to_matrix(&p, p_array);
+
+  // enforce symmetry of the covariance matrix, and ensure the values stay bounded
+  for (int i=0; i<KC_STATE_DIM; i++) {
+    for (int j=i; j<KC_STATE_DIM; j++) {
+      float p_val = 0.5f*p_array[i][j] + 0.5f*p_array[j][i];
+      if (isnan(p_val) || p_val > MAX_COVAR) {
+        p_array[i][j] = p_array[j][i] = MAX_COVAR;
+      } else if ( i==j && p_val < MIN_COVAR ) {
+        p_array[i][j] = p_array[j][i] = MIN_COVAR;
+      } else {
+        p_array[i][j] = p_array[j][i] = p_val;
+      }
+    }
+  }
+  _out->p = p;
+}
+
+#include "supervisor.h"
+void Mathext__supervisor_is_flying_step(Mathext__supervisor_is_flying_out* _out) {
+  _out->ok = supervisorIsFlying();
 }
